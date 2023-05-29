3b17a50061f9b575be481038153c134a0f6d6049
sumitsharma372 <sksharma372@kgpian.iitkgp.ac.in>
2023-05-30 03:11:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import RoutesHandler from './routes';
+import { ToastContainer } from 'react-toastify';
+
+import axios from 'axios';
+axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App font-poppins">
+      <ToastContainer />
+      <Navbar />
+      <RoutesHandler />
     </div>
   );
 }
